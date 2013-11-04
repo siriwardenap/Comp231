20e1d65770698e97f4c40b0e3d67ef6abaf6c12e
HuifengZhou <jjje2003@gmail.com>
2013-11-04 10:47:50 -0500

    www

---
+++ CulinaryDelight-V3/CulinaryDelight/CulinaryDelight/Account/RegisterCustomer.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Membership.OpenAuth;
+using System.Web.Security;
+
+public partial class Account_RegisterCustomer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void RegisterUser_CreatedUser(object sender, EventArgs e)
+    {
+        ViewState["__AntiXsrfUserName"] = Context.User.Identity.Name ?? String.Empty;
+        Roles.AddUserToRole((sender as CreateUserWizard).UserName, "Customers");
+    }
+}
