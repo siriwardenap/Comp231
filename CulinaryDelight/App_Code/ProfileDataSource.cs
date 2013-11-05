905d2a13454c4fbff32fa1e19d7720b4f445ed6b
HuifengZhou <jjje2003@gmail.com>
2013-11-05 12:16:02 -0500

    |HF s development as at 5th Nov

---
+++ CulinaryDelight/App_Code/ProfileDataSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// Summary description for ProfileDataSource
+/// </summary>
+public class ProfileDataSource
+{
+	public ProfileDataSource()
+	{
+		
+	}
+
+    public List<ProfileWrapper> GetData()
+    {
+        List<ProfileWrapper> data = new List<ProfileWrapper>();
+        data.Add(new ProfileWrapper());
+        return data;
+    }
+
+    public void UpdateData(ProfileWrapper newData)
+    {
+        newData.UpdateProfile();
+    }
+
+}
